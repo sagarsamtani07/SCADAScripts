30725849b84189f97c3d773c0cf4aca7ab9ca854
Eric <egross00@live.com>
2015-04-26 00:10:07 -0700

    update

--- NVDtester.py
+++ NVDtester.py
@@ -1,15 +1,16 @@
+# __author__ = 'Eric Gross'
 # code to test Shodan entries against NVD database entries 
 
 import pymysql
 
 
-shodandb = pymysql.connect(host="128.196.27.147",  # your host, usually localhost
-                     user="ShodanTeam",  # your username
-                     passwd="Sh0d@n7e",  # your password
-                     db="shodan",
-                     charset='utf8',
-                     cursorclass=pymysql.cursors.DictCursor,
-                     autocommit=True)  # name of the data base
+#shodandb = pymysql.connect(host="128.196.27.147",  # your host, usually localhost
+#                     user="ShodanTeam",  # your username
+#                     passwd="Sh0d@n7e",  # your password
+#                     db="shodan",
+#                     charset='utf8',
+#                     cursorclass=pymysql.cursors.DictCursor,
+#                     autocommit=True)  # name of the data base
 
 nvddb = pymysql.connect(host="128.196.27.147",  # your host, usually localhost
                      user="ShodanTeam",  # your username
@@ -26,25 +27,50 @@ vulnerablesystems = pymysql.connect(host="128.196.27.147",  # your host, usually
                      charset='utf8',
                      cursorclass=pymysql.cursors.DictCursor,
                      autocommit=True)  # name of the data base
-
-try:
-
-    with shodandb.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
-        cursor.execute(sql, ('%scada%',))
-        result = cursor.fetchone()
-        print(result)
-finally:
-    shodandb.close()
     
 try:
 
-    with nvddb.cursor() as cursor:
+    with nvddb.cursor() as cursornv:
         # Read a single record
         sql = "SELECT `vendor`, `product`, `version` FROM `nvdvuln` WHERE `vendor` like %s or `product` like %s"
-        cursor.execute(sql, ('%power%','%power%',))
-        result = cursor.fetchone()
+        cursornv.execute(sql, ('%power%','%power%',))
+        result = cursornv.fetchone()
         print(result)
+#        
+#    with shodandb.cursor() as cursorsdb:
+#        # Read a single record
+#        sql = "SELECT `ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
+#        cursorsdb.execute(sql, ('%scada%',))
+#        for r in shodandb.fetchall():
+#            print(r)
+#        result = cursorsdb.fetchone()
+#        print(result)
+
+    with vulnerablesystems.cursor() as cursornv:
+        # Read a single record
+        sql = "SELECT `vendor`, `product`, `version` FROM `nvdvuln` WHERE `vendor` like %s or `product` like %s"
+        cursornv.execute(sql, ('%power%','%power%',))
+        result = cursornv.fetchone()
+        print(result)
+        
+    with vulnerablesystems.cursor() as cursorsdb:
+        # Read a single record
+        sql = "SELECT `ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
+        cursorsdb.execute(sql, ('%scada%',))
+        for r in shodandb.fetchall():
+            print(r)
+        result = cursorsdb.fetchone()
+        print(result)
+
+    try:
+        with vulnerablesystems.cursor() as cursorvs:
+            # Create a new record
+            sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `Score`) VALUES (%s, %s, %s, %s)"
+            cursorvs.execute(sql, ('webmaster@python.org', 'very-secret', 'very-secret', 'very-secret'))
+    except: 
+        print('duplicate entry: rollback')
+
 finally:
     nvddb.close()
+#    shodandb.close()
+    vulnerablesystems.close()

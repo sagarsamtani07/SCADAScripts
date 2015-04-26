c2c5d6e3356635a18c65e6d5d3797158fa2d4743
Eric <egross00@live.com>
2015-04-26 00:57:13 -0700

    added db connect

--- NVDtester.py
+++ NVDtester.py
@@ -32,7 +32,7 @@ vulnerablesystems = pymysql.connect(host="128.196.27.147",  # your host, usually
 try:
 
     with nvddb.cursor() as cursornv:
-        # Read a single record
+        # Read all records from NVD DB
         sql = "SELECT `cvd_id`,`vendor`, `product`, `version` FROM `nvdvuln`"
         cursornv.execute(sql)
         result = cursornv.fetchall()
@@ -41,23 +41,26 @@ try:
             vendor = r["cvd_id"]
             product = r["cvd_id"]
             version = r["cvd_id"]
+        print("executed nvd cursor")
 
     with shodandb.cursor() as cursorsdb:
-        # Read a single record
+        # Read all records from SFS SCADA db
         sql = "SELECT `ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
         cursorsdb.execute(sql, ('%scada%',))
         result = cursorsdb.fetchall()
         for r in result:
-            cvid = r["ip_str"]
-            vendor = r["data"]
-
-    try:
-        with vulnerablesystems.cursor() as cursorvs:
-            # Create a new record
-            sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `Score`) VALUES (%s, %s, %s, %s)"
-            cursorvs.execute(sql, ('webmaster@python.org', 'very-secret', 'very-secret', 'very-secret'))
-    except: 
-        print('duplicate entry: rollback')
+            ip_str = r["ip_str"]
+            data = r["data"]
+        print("executed Shodan cursor")
+    if((vendor in data) or (product in data) and (version in data)): 
+        try:
+            with vulnerablesystems.cursor() as cursorvs:
+                # Create a new record in Vulnerable systems
+                sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `Score`) VALUES (%s, %s, %s, %s)"
+                cursorvs.execute(sql, ('webmaster@python.org', 'very-secret', 'very-secret', 'very-secret'))
+            print("Vulnerable system found!")
+        except: 
+            print('duplicate entry: rollback')
 
 finally:
     nvddb.close()

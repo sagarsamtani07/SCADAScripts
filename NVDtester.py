be98654c08d676d08f258c59a7f9ac5bcef6ad93
Eric <egross00@live.com>
2015-04-26 01:31:24 -0700

    final nvd tester

--- NVDtester.py
+++ NVDtester.py
@@ -27,40 +27,45 @@ vulnerablesystems = pymysql.connect(host="128.196.27.147",  # your host, usually
                      charset='utf8',
                      cursorclass=pymysql.cursors.DictCursor,
                      autocommit=True)  # name of the data base
-
     
-try:
+try:  
 
     with nvddb.cursor() as cursornv:
         # Read all records from NVD DB
-        sql = "SELECT `cvd_id`,`vendor`, `product`, `version` FROM `nvdvuln`"
+        sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln`"
         cursornv.execute(sql)
         result = cursornv.fetchall()
         for r in result:
             cvid = r["cvd_id"]
-            vendor = r["cvd_id"]
-            product = r["cvd_id"]
-            version = r["cvd_id"]
-        print("executed nvd cursor")
-
-    with shodandb.cursor() as cursorsdb:
-        # Read all records from SFS SCADA db
-        sql = "SELECT `ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
-        cursorsdb.execute(sql, ('%scada%',))
-        result = cursorsdb.fetchall()
-        for r in result:
-            ip_str = r["ip_str"]
-            data = r["data"]
-        print("executed Shodan cursor")
-    if((vendor in data) or (product in data) and (version in data)): 
-        try:
-            with vulnerablesystems.cursor() as cursorvs:
-                # Create a new record in Vulnerable systems
-                sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `Score`) VALUES (%s, %s, %s, %s)"
-                cursorvs.execute(sql, ('webmaster@python.org', 'very-secret', 'very-secret', 'very-secret'))
-            print("Vulnerable system found!")
-        except: 
-            print('duplicate entry: rollback')
+            vendor = r["vendor"]
+            product = r["product"]
+            version = r["version"]
+            score = r["Score"]
+            
+            with shodandb.cursor() as cursorsdb:
+                # Read all records from SFS SCADA db
+                sql = "SELECT `ID`,`ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
+                cursorsdb.execute(sql, ('%scada%',))
+                result2 = cursorsdb.fetchall()
+                for r in result2:
+                    ShodanID = r["ID"]
+                    ip_str = r["ip_str"]
+                    data = r["data"]
+                    print("executed Shodan cursor")  
+            
+                    print(product)
+                    print(data)
+                    print(version)
+                    
+                    if(product in data) and (version in data): #or (product in data) and (version in data)): 
+                        try:
+                            with vulnerablesystems.cursor() as cursorvs:
+                                # Create a new record in Vulnerable systems
+                                sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `Score`) VALUES (%s, %s, %s, %s)"
+                                cursorvs.execute(sql, (ShodanID, ip_str, cvid, score))
+                            print("Vulnerable system found!")
+                        except: 
+                            print('duplicate entry: rollback')
 
 finally:
     nvddb.close()

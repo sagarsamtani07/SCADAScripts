78011b54d7729f4f8efd4c2e3e9d50dc7258217d
Eric <egross00@live.com>
2015-04-27 11:32:25 -0700

    add logging

--- NVDtester.py
+++ NVDtester.py
@@ -30,50 +30,54 @@ vulnerablesystems = pymysql.connect(host="128.196.27.147",  # your host, usually
                      charset='utf8',
                      cursorclass=pymysql.cursors.DictCursor,
                      autocommit=True)  # name of the data base
-    
-try:  
-        
-    with nvddb.cursor() as cursornv:
-        # Read all records from NVD DB
-        sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln` where `cvd_id` like '%2015%' or `cvd_id` like '%2014%' or `cvd_id` like '%2013%' or `cvd_id` like '%2012%' and CHAR_LENGTH(product) > 4"
-        cursornv.execute(sql)
-        result = cursornv.fetchall()
-        
-        for r in result:
-            cvid = r["cvd_id"]
-            vendor = r["vendor"]
-            product = r["product"]
-            version = str_replace(r["version"])
-            score = r["Score"]
+
+
+with open("C:/Users/Gross/Desktop/NVDtesterLogs/log.txt", "w+") as log:
+
+    try:  
             
-            datatest = "%" + product + "%" + version + "%"
+        with nvddb.cursor() as cursornv:
+            # Read all records from NVD DB
+            sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln` where `cvd_id` like '%2015%' or `cvd_id` like '%2014%' or `cvd_id` like '%2013%' and CHAR_LENGTH(product) > 4"# or `cvd_id` like '%2012%' and CHAR_LENGTH(product) > 4"
+            cursornv.execute(sql)
+            result = cursornv.fetchall()
             
-            print(datatest)
-            
-            with shodandb.cursor() as cursorsdb:
-                # Read all records from SFS SCADA db
-                sql = "SELECT `ID`,`ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
-                cursorsdb.execute(sql, (datatest,))
-                result2 = cursorsdb.fetchall()
+            for r in result:
+                cvid = r["cvd_id"]
+                vendor = r["vendor"]
+                product = r["product"]
+                version = str_replace(r["version"])
+                score = r["Score"]
+                
+                datatest = "%" + product + "%" + version + "%"
+                
+                print(datatest)
+                
+                with shodandb.cursor() as cursorsdb:
+                    # Read all records from SFS SCADA db
+                    sql = "SELECT `ID`,`ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
+                    cursorsdb.execute(sql, (datatest,))
+                    result2 = cursorsdb.fetchall()
 
-                for r in result2:
-                    ShodanID = r["ID"]
-                    ip_str = r["ip_str"]
-                    data = r["data"]
-                    print("executed Shodan cursor")  
+                    for r in result2:
+                        ShodanID = r["ID"]
+                        ip_str = r["ip_str"]
+                        data = r["data"]
+                        print("executed Shodan cursor")  
 
-                    print(cvid + "on" + ip_str)
+                        print(cvid + "on" + ip_str)
 
-                    try:
-                        with vulnerablesystems.cursor() as cursorvs:
-                            # Create a new record in Vulnerable systems
-                            sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                            cursorvs.execute(sql, (ShodanID, ip_str, cvid, vendor, product, version, score))
-                        print("Vulnerable system found!")
-                    except: 
-                        print('duplicate entry: rollback')
+                        try:
+                            with vulnerablesystems.cursor() as cursorvs:
+                                # Create a new record in Vulnerable systems
+                                sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                                cursorvs.execute(sql, (ShodanID, ip_str, cvid, vendor, product, version, score))
+                            print("Vulnerable system found!")
+                        except Exception as err:
+                                log.write("Unsolved exception: {0}, at CVID {1}, ip {2}\n".format(err, cvid, ip_str))
+                                log.flush()
 
-finally:
-    nvddb.close()
-    shodandb.close()
-    vulnerablesystems.close()
+    finally:
+        nvddb.close()
+        shodandb.close()
+        vulnerablesystems.close()

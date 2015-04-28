1f71debd6d92cf7b21e4a6ae22ea4f227e2adb74
Eric <egross00@live.com>
2015-04-27 22:05:37 -0700

    Revert "This version holds everything in loval memory"
    This reverts commit 0b06615051c72f9c48d070993d22b99bcbcf01f4.

--- NVDtesterReverse.py
+++ NVDtesterReverse.py
@@ -38,65 +38,74 @@ vulnerablesystems = pymysql.connect(host="128.196.27.147",  # your host, usually
                      autocommit=True)  # name of the data base
 
 
-with open("E:/EricGross-PC/Users/Eric/Desktop/log.txt", "w+") as log:
+with open("C:/Users/Gross/Desktop/NVDtesterLogs/log.txt", "w+") as log:
 
     try:  
-        
-        with shodandb.cursor() as cursorsdb:
-                sql = "SELECT `ID`,`ip_str`, `data` FROM `sy_sfs_scadashodan`"
-                cursorsdb.execute(sql)
-                result2 = cursorsdb.fetchall()           
-                    
+            
         with nvddb.cursor() as cursornv:
-                # Read all records from NVD DB
-                #sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln` where `product` = 'windows_xp'"
-                sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln` where `cvd_id` like '%2015%' or `cvd_id` like '%2014%' or `cvd_id` like '%2013%' or `cvd_id` like '%2012%' and CHAR_LENGTH(product) > 4"
-                cursornv.execute(sql)
-                result = cursornv.fetchall() 
-                            
-                for r in result2:
+            # Read all records from NVD DB
+            #sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln` where `product` = 'windows_xp'"
+            sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln` where `cvd_id` like '%2015%' or `cvd_id` like '%2014%' or `cvd_id` like '%2013%' or `cvd_id` like '%2012%' and CHAR_LENGTH(product) > 4"
+            cursornv.execute(sql)
+            result = cursornv.fetchall()
+            
+            for r in result:
+                cvid = r["cvd_id"]
+                vendor = r["vendor"]
+                product = r["product"]
+                versorig = r["version"]
+                version = str_replace(versorig)
+                score = r["Score"]
+                
+                datatest = "%" + product + "%" + version + "%"
+                
+                print(datatest)
+                
+                with shodandb.cursor() as cursorsdb:
+                    # Read all records from SFS SCADA db
+                    sql = "SELECT `ID`,`ip_str`, `data` FROM `sy_sfs_scadashodan` WHERE `data` like %s"
+                    cursorsdb.execute(sql, (datatest,))
+                    result2 = cursorsdb.fetchall()
+
+                    for r in result2:
                         ShodanID = r["ID"]
                         ip_str = r["ip_str"]
                         data = r["data"]
+                        print("executed Shodan cursor on " + ip_str)
+                        
+                        print(version)
+                        m = re.search("([^(0-9)][^(\.)])(%s)([^0-9])"%version, data)
 
-                        for r in result:
-                            cvid = r["cvd_id"]
-                            vendor = r["vendor"]
-                            product = r["product"]
-                            versorig = r["version"]
-                            version = str_replace(versorig)
-                            score = r["Score"]
+                        newprod = str_replaceprod(product)
+                        print(newprod)
+                                                              
+                        l = re.search("(%s)"%newprod, data)
+                        
+                        if version == "":
+                            try:
+                                with vulnerablesystems.cursor() as cursorvs:
+                                    # Create a new record in Vulnerable systems
+                                    sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                                    cursorvs.execute(sql, (ShodanID, ip_str, cvid, vendor, product, versorig, score))
+                                print("Vulnerable system found!")
+                                print(cvid + " on " + ip_str)
+                            except Exception as err:
+                                    log.write("Unsolved exception: {0}, at CVID {1}, ip {2}\n".format(err, cvid, ip_str))
+                                    log.flush()
 
-                            v = re.search("([^(0-9)][^(\.)])(%s)([^0-9])"%version, data)
-
-                            newprod = str_replaceprod(product)
-                            p = re.search("(%s)"%newprod, data)
-
-                            if p and version == "":
-                                try:
-                                    with vulnerablesystems.cursor() as cursorvs:
-                                        # Create a new record in Vulnerable systems
-                                        sql = "INSERT INTO `nvdvulntest` (`ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                                        cursorvs.execute(sql, (ShodanID, ip_str, cvid, vendor, product, versorig, score))
-                                    print("Vulnerable system found!")
-                                    print(cvid + " on " + ip_str)
-                                except Exception as err:
-                                        log.write("Unsolved exception: {0}, at CVID {1}, ip {2}\n".format(err, cvid, ip_str))
-                                        log.flush()
-
-                            if v and p:
-                                try:
-                                    with vulnerablesystems.cursor() as cursorvs:
-                                        # Create a new record in Vulnerable systems
-                                        sql = "INSERT INTO `nvdvulntest` (`ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                                        cursorvs.execute(sql, (ShodanID, ip_str, cvid, vendor, product, versorig, score))
-                                    print("Vulnerable system found!")
-                                    print(cvid + " on " + ip_str)
-                                except Exception as err:
-                                        log.write("Unsolved exception: {0}, at CVID {1}, ip {2}\n".format(err, cvid, ip_str))
-                                        log.flush()
-                            else:
-                                pass
+                        if m and l:
+                            try:
+                                with vulnerablesystems.cursor() as cursorvs:
+                                    # Create a new record in Vulnerable systems
+                                    sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                                    cursorvs.execute(sql, (ShodanID, ip_str, cvid, vendor, product, versorig, score))
+                                print("Vulnerable system found!")
+                                print(cvid + " on " + ip_str)
+                            except Exception as err:
+                                    log.write("Unsolved exception: {0}, at CVID {1}, ip {2}\n".format(err, cvid, ip_str))
+                                    log.flush()
+                        else:
+                            print("incorrect MySQL version match")
 
     finally:
         nvddb.close()

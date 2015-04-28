f31a20821d8605f8917c23a258de4da0bd2fffd2
HongyiZhu <hongyi.zhu.21@gmail.com>
2015-04-27 18:25:40 -0700

    Modified some coding format to enhance readability

--- NVDtester.py
+++ NVDtester.py
@@ -4,38 +4,41 @@
 import pymysql
 import re
 
+
 def str_replace(s):
-    return str(s).replace("-","").replace("None","").replace(".", "\.")
+    return str(s).replace("-", "").replace("None", "").replace(".", "\.")
+
 
 def str_replaceteststr(d):
     return str(d).replace(".", "\\.")
+
 
 def str_replaceprod(p):
     return str(p).replace("_", ".").replace("/", ".").replace("\\", ".").replace("-", ".").replace(" ", ".")
 
 shodandb = pymysql.connect(host="128.196.27.147",  # your host, usually localhost
-                     user="ShodanTeam",  # your username
-                     passwd="Sh0d@n7e",  # your password
-                     db="shodan",
-                     charset='utf8',
-                     cursorclass=pymysql.cursors.DictCursor,
-                     autocommit=True)  # name of the data base
+                           user="ShodanTeam",  # your username
+                           passwd="Sh0d@n7e",  # your password
+                           db="shodan",
+                           charset='utf8',
+                           cursorclass=pymysql.cursors.DictCursor,
+                           autocommit=True)  # name of the data base
 
 nvddb = pymysql.connect(host="128.196.27.147",  # your host, usually localhost
-                     user="ShodanTeam",  # your username
-                     passwd="Sh0d@n7e",  # your password
-                     db="nvddb",
-                     charset='utf8',
-                     cursorclass=pymysql.cursors.DictCursor,
-                     autocommit=True)  # name of the data base
+                        user="ShodanTeam",  # your username
+                        passwd="Sh0d@n7e",  # your password
+                        db="nvddb",
+                        charset='utf8',
+                        cursorclass=pymysql.cursors.DictCursor,
+                        autocommit=True)  # name of the data base
 
 vulnerablesystems = pymysql.connect(host="128.196.27.147",  # your host, usually localhost
-                     user="ShodanTeam",  # your username
-                     passwd="Sh0d@n7e",  # your password
-                     db="vulnerablesystems",
-                     charset='utf8',
-                     cursorclass=pymysql.cursors.DictCursor,
-                     autocommit=True)  # name of the data base
+                                    user="ShodanTeam",  # your username
+                                    passwd="Sh0d@n7e",  # your password
+                                    db="vulnerablesystems",
+                                    charset='utf8',
+                                    cursorclass=pymysql.cursors.DictCursor,
+                                    autocommit=True)  # name of the data base
 
 
 with open("C:/Users/Gross/Desktop/NVDtesterLogs/log.txt", "w+") as log:
@@ -44,8 +47,12 @@ with open("C:/Users/Gross/Desktop/NVDtesterLogs/log.txt", "w+") as log:
             
         with nvddb.cursor() as cursornv:
             # Read all records from NVD DB
-            #sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln` where `product` = 'windows_xp'"
-            sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln` where `cvd_id` like '%2015%' or `cvd_id` like '%2014%' or `cvd_id` like '%2013%' or `cvd_id` like '%2012%' and CHAR_LENGTH(product) > 4"
+            # sql = "SELECT `cvd_id`,`vendor`, `product`, `version`, `Score`
+            # FROM `nvdvuln` where `product` = 'windows_xp'"
+            sql = """SELECT `cvd_id`,`vendor`, `product`, `version`, `Score` FROM `nvdvuln`
+                     WHERE `cvd_id` LIKE '%2015%' OR `cvd_id` LIKE '%2014%'
+                     OR `cvd_id` LIKE '%2013%' OR `cvd_id` LIKE '%2012%'
+                     AND CHAR_LENGTH(product) > 4;"""
             cursornv.execute(sql)
             result = cursornv.fetchall()
             
@@ -67,25 +74,29 @@ with open("C:/Users/Gross/Desktop/NVDtesterLogs/log.txt", "w+") as log:
                     cursorsdb.execute(sql, (datatest,))
                     result2 = cursorsdb.fetchall()
 
-                    for r in result2:
-                        ShodanID = r["ID"]
-                        ip_str = r["ip_str"]
-                        data = r["data"]
+                    for r2 in result2:
+                        ShodanID = r2["ID"]
+                        ip_str = r2["ip_str"]
+                        data = r2["data"]
                         print("executed Shodan cursor on " + ip_str)
                         
                         print(version)
-                        m = re.search("([^(0-9)][^(\.)])(%s)([^0-9])"%version, data)
+                        m = re.search("([^(0-9)][^(\.)])(%s)([^0-9])" % version, data)
 
                         newprod = str_replaceprod(product)
                         print(newprod)
                                                               
-                        l = re.search("(%s)"%newprod, data)
+                        l = re.search("(%s)" % newprod, data)
                         
                         if version == "":
                             try:
                                 with vulnerablesystems.cursor() as cursorvs:
                                     # Create a new record in Vulnerable systems
-                                    sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                                    sql = """INSERT INTO `nvdvuln` (
+                                             `ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`
+                                             ) VALUES (
+                                             %s, %s, %s, %s, %s, %s, %s
+                                             );"""
                                     cursorvs.execute(sql, (ShodanID, ip_str, cvid, vendor, product, versorig, score))
                                 print("Vulnerable system found!")
                                 print(cvid + " on " + ip_str)
@@ -97,7 +108,11 @@ with open("C:/Users/Gross/Desktop/NVDtesterLogs/log.txt", "w+") as log:
                             try:
                                 with vulnerablesystems.cursor() as cursorvs:
                                     # Create a new record in Vulnerable systems
-                                    sql = "INSERT INTO `nvdvuln` (`ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                                    sql = """INSERT INTO `nvdvuln` (
+                                             `ShodanID`, `ipaddr`, `CVE-ID`, `vendor`,`product`,`version`, `score`
+                                             ) VALUES (
+                                             %s, %s, %s, %s, %s, %s, %s
+                                             );"""
                                     cursorvs.execute(sql, (ShodanID, ip_str, cvid, vendor, product, versorig, score))
                                 print("Vulnerable system found!")
                                 print(cvid + " on " + ip_str)

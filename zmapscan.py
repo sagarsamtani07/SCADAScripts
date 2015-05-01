0ce67a6e7a16350230fdb3d03b7d03f7021ec6e2
HongyiZhu <hongyi.zhu.21@gmail.com>
2015-04-30 18:06:57 -0700

    This code can work, but really slow

--- zmapscan.py
+++ zmapscan.py
@@ -52,40 +52,58 @@ for (result) in ip_result:
     id = result[1]
 
     proc_list = []
-
-    # creating threads to scan all the ports of one ip
-    for port in port_list:
-        proc_list.append(subprocess.Popen(["zmap", "-p", str(port), "{0} -o -".format(ip)],
-                                          stdout=PIPE,
-                                          stderr=PIPE))
-    print("thread created for ip: {0}".format(ip))
-
-    # waiting for the scan
-    while True:
-        bigflag = True
-        for proc in proc_list:
-            flag = proc.poll()
-            if flag is None:
-                bigflag = False
-                time.sleep(1)
-                break
-        if bigflag:
-            break
-
+    
+    # intitalize two lists
+    port_check_list = [1 for i in range(len(port_list))]
+    proc_list = [None for i in range(len(port_list))]
     port_string_list = []
 
-    # read scaning results
-    for i in range(len(proc_list)):
-        proc = proc_list[i]
-        stdou, stderr = proc.communicate()
-        if stderr.decode("utf8").strip().find("hits: 100.00%") != -1:
-            port_string_list.append(str(port_list[i]))
-        else:
-            pass
+    print("Scan IP: " + ip)
+    while 1 in port_check_list: # while not all scan is finished
+        for i in range(len(port_check_list)):
+            if port_check_list[i] == 1: #port scan not finished
+                proc_list[i] = subprocess.Popen(["zmap", "-p", str(port_list[i]), "{0} -o -".format(ip)],
+                                              stdout=PIPE,
+                                              stderr=PIPE)  # create a scanning thread for the port 
+
+        # creating threads to scan all the ports of one ip
+        # for port in port_list:
+        #    proc_list.append(subprocess.Popen(["zmap", "-p", str(port), "{0} -o -".format(ip)],
+        #                                       stdout=PIPE,
+        #                                       stderr=PIPE))
+        # print("thread created for ip: {0}".format(ip))
+
+        # waiting for the scan
+        while True:
+            bigflag = True
+            for i in range(len(port_list)):
+                if port_check_list[i] == 1: # check this port
+                    proc = proc_list[i]                    
+                    flag = proc.poll()
+                    if flag is None:
+                        bigflag = False
+                        time.sleep(1)
+                        break
+            if bigflag:
+                break
+
+        # read scaning results
+        for i in range(len(port_check_list)):
+            if port_check_list[i] == 1: # port response not processed
+                proc = proc_list[i]
+                stdou, stderr = proc.communicate()
+                if stderr.decode("utf8").strip().find("hits: 100.00%") != -1:
+                    port_string_list.append(str(port_list[i]))
+                    port_check_list[i] = 0
+                elif stderr.decode("utf8").strip().find("0.00%") == -1:
+                    pass
+                else:
+                    port_check_list[i] = 0
+    
     port_string = ";".join(port_string_list)
+    print("Open Ports: " + port_string)    
 
     if port_string != "":
-        print(port_string)
         try:
             sql = """
                   INSERT INTO `shodan`.`zmap_verified_devices` (
@@ -94,7 +112,7 @@ for (result) in ip_result:
                   '%s', '%s', '%s', '%s'
                   );
                   """ % (ip, time.time(), port_string, id)
-            print(sql)
+            # print(sql)
             cur_ou.execute(sql)
         except pymysql.err.IntegrityError as err:
             log.write("IntegrityError for {0}: {1}\n".format(ip, err))

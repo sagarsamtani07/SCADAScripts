eac9c14a44ef032525d456a883dc8e34ca5cd780
sagarsamtani07 <sagars@email.arizona.edu>
2015-09-15 22:48:11 -0700

    Update ShodanJsonParser.py

--- ShodanJsonParser.py
+++ ShodanJsonParser.py
@@ -13,6 +13,7 @@ db = pymysql.connect(host="128.196.27.147", # your host, usually localhost
                          db="shodan", # name of the data base
                          charset='utf8',
                          autocommit=True)
+
 def str_replace(s):
     return str(s).replace("\\", "\\\\").replace("'", "\\'")
 
@@ -28,6 +29,7 @@ iplist = [row[0] for row in results]
 APIURL =  "http://ip-api.com/json/"
 
 counter = 0
+recordNo = 0
 for ip in iplist:
     APICALL = APIURL + ip
     response = urllib.request.urlopen(APICALL)
@@ -37,20 +39,20 @@ for ip in iplist:
 
     # since JSON will always be returned, it will either have success or failure
     if status == 'success':
-        as_info = (str_replace(json_data['as']))
-        city = (str_replace(json_data['city']))
-        country = (str_replace(json_data['country']))
-        countryCode = (str_replace(json_data['countryCode']))
-        isp = (str_replace(json_data['isp']))
-        lat = (str_replace(json_data['lat']))
-        lon = (str_replace(json_data['lon']))
-        org = (str_replace(json_data['org']))
-        ip_addr = (str_replace(json_data['query']))
-        region = (str_replace(json_data['region']))
-        regionName = (str_replace(json_data['regionName']))
-        status = (str_replace(json_data['status']))
-        timezone = (str_replace(json_data['timezone']))
-        zip = (str_replace(json_data['zip']))
+        as_info = str_replace((json_data['as']))
+        city = str_replace((json_data['city']))
+        country = str_replace((json_data['country']))
+        countryCode = str_replace((json_data['countryCode']))
+        isp = str_replace((json_data['isp']))
+        lat = str_replace((json_data['lat']))
+        lon = str_replace((json_data['lon']))
+        org = str_replace((json_data['org']))
+        ip_addr = str_replace((json_data['query']))
+        region = str_replace((json_data['region']))
+        regionName = str_replace((json_data['regionName']))
+        status = str_replace((json_data['status']))
+        timezone = str_replace((json_data['timezone']))
+        zip = str_replace((json_data['zip']))
 
         #insert into the database
         string = """INSERT INTO vulnerablesystems_locations
@@ -59,8 +61,9 @@ for ip in iplist:
                  (as_info ,city,country,countryCode,isp,lat,lon,org,ip_addr,region,regionName,status,timezone,zip)
 
         string = string.replace("'None'", "null")
-        print (string)
         cur.execute(string)
+        recordNo += 1
+        print (recordNo)
         #cur.commit()
     else:
         pass
